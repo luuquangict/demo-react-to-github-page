928549730b71429031865a5e83b47f1a993bc419
Nguyễn Văn Quang <luuquangict@gmail.com>
2022-05-10 16:46:32 +0700

    Edit 2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br>Hello Vercel
+          Edit <code>src/App.js</code> and save to reload. Ahihi          
         </p>
         <a
           className="App-link"
